a52711faf54c5ac2d0fde92c1bac8bd4d51ff77f
deadprogrammer <ron.evans@gmail.com>
2011-09-02 09:04:04 -0700

    Bunch of refactoring of setup and installation scripts

--- install.sh
+++ install.sh
@@ -1,31 +1,80 @@
 #!/bin/sh
 
-# download/build needed resources
 echo "Make sure your download and install the Platypus installer from here: http://www.sveinbjorn.org/platypus"
 
-if [ ! -f "build" ]
-then
-	mkdir build
-fi
+create_dirs() {
+  if [ ! -d "build" ]
+	then
+		mkdir build
+	fi
 
-if [ ! -f "resources" ]
-then
-	mkdir resources
-fi
+	if [ ! -d "resources" ]
+	then
+		mkdir resources
+	fi
+}
 
-if [ ! -f "resources/qt-mac-opensource-4.7.3.dmg" ]
-then
-	curl "http://get.qt.nokia.com/qt/source/qt-mac-opensource-4.7.3.dmg" > "resources/qt-mac-opensource-4.7.3.dmg"
-fi
+check_qt() {
+	if [ ! -f "resources/qt-mac-opensource-4.7.3.dmg" ]
+	then
+		curl "http://get.qt.nokia.com/qt/source/qt-mac-opensource-4.7.3.dmg" > "resources/qt-mac-opensource-4.7.3.dmg"
+	fi
+}
 
-if [ ! -f "resources/ruby-1.9.2-p290.universal.tar.gz" ]
-then
+check_git() {
+	if [ ! -f "resources/git-1.7.6-i386-snow-leopard.dmg" ]
+	then
+		curl "http://git-osx-installer.googlecode.com/files/git-1.7.6-i386-snow-leopard.dmg" > "resources/git-1.7.6-i386-snow-leopard.dmg"
+	fi
+}
+
+get_yaml() {
+	if [ ! -f "build/yaml-0.1.4.tar.gz" ]
+	then
+		curl "http://pyyaml.org/download/libyaml/yaml-0.1.4.tar.gz" > "build/yaml-0.1.4.tar.gz"
+	fi
+}
+
+build_yaml() {
+	cd build
+	if [ ! -d "yaml" ]
+	then
+		mkdir yaml
+	fi
+	cd yaml
+	yamldir="$(pwd)"
+	cd ..
+	tar -xvzf yaml-0.1.4.tar.gz
+	cd yaml-0.1.4
+	CFLAGS="-arch i386 -arch x86_64"
+	export CFLAGS
+	LDFLAGS="-arch i386 -arch x86_64"
+	export LDFLAGS
+	./configure --prefix="$yamldir" --disable-dependency-tracking
+	make
+	make install
+	cd ../..
+	tar cvzf resources/yaml-0.1.4.universal.tar.gz build/yaml
+}
+
+check_yaml() {
+	if [ ! -f "resources/yaml-0.1.4.universal.tar.gz" ]
+	then
+		get_yaml
+		build_yaml
+	fi
+}
+
+get_ruby() {
 	if [ ! -f "build/ruby-1.9.2-p290.tar.gz" ]
 	then
 		curl "http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p290.tar.gz" > "build/ruby-1.9.2-p290.tar.gz"
 	fi
+}
+
+build_ruby() {
 	cd build
-	if [ ! -f "ruby" ]
+	if [ ! -d "ruby" ]
 	then
 		mkdir ruby
 	fi
@@ -34,9 +83,23 @@ then
 	cd ..
 	tar -xvzf ruby-1.9.2-p290.tar.gz
 	cd ruby-1.9.2-p290
-	./configure --with-arch=x86_64,i386 --prefix="$rubydir"
+	./configure --with-arch=x86_64,i386 --prefix="$rubydir" --with-libyaml-dir="$yamldir"
 	make
 	make install
-	cd ../..
-	tar cvzf resources/ruby-1.9.2-p290.universal.tar.gz build/ruby
-fi
+	cd ..
+	tar cvzf "../resources/ruby-1.9.2-p290.universal.tar.gz" ruby
+}
+
+check_ruby() {
+	if [ ! -f "resources/ruby-1.9.2-p290.universal.tar.gz" ]
+	then
+		get_ruby
+		build_ruby
+	fi
+}
+
+create_dirs
+check_qt
+check_git
+check_yaml
+check_ruby
--- installer.sh
+++ installer.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+INSTALLDIR="/Applications/KidsRuby"
 
 # determine OSX version
 TIGER=4
@@ -7,29 +8,63 @@ SNOW_LEOPARD=6
 LION=7
 osx_version=$(sw_vers -productVersion | awk 'BEGIN {FS="."}{print $2}')
 if [ $osx_version -eq $LEOPARD -o $osx_version -eq $SNOW_LEOPARD -o $osx_version -eq $LION ]; then
-	echo Supported OS
+	echo "Starting KidsRuby install..."
 else
-	echo Unsupported OS
+	echo "Sorry, KidsRuby is not currently supported on your operating system."
+	exit
 fi
 
-# install qt libs
-hdiutil attach qt-mac-opensource-4.7.3.dmg
-/usr/sbin/installer -verbose -pkg "/Volumes/Qt 4.7.3/Qt.mpkg" -target /
-hdiutil detach "/Volumes/Qt 4.7.3"
+create_install_dir() {
+	echo "Creating installation directory..."
+	if [ ! -d "$INSTALLDIR" ]
+	then
+		mkdir "$INSTALLDIR"
+	fi
+}
 
-# Install git
+install_qt() {
+	echo "Installing Qt..."
+	hdiutil attach qt-mac-opensource-4.7.3.dmg
+	/usr/sbin/installer -verbose -pkg "/Volumes/Qt 4.7.3/Qt.mpkg" -target /
+	hdiutil detach "/Volumes/Qt 4.7.3"
+}
 
-# Install ruby 1.9.2 universal binary
+install_git() {
+	echo "Installing git..."
+	hdiutil attach git-1.7.6-i386-snow-leopard.dmg
+	/usr/sbin/installer -verbose -pkg "/Volumes/Git 1.7.6 i386 Snow Leopard/git-1.7.6-i386-snow-leopard.pkg" -target /
+	hdiutil detach "/Volumes/Git 1.7.6 i386 Snow Leopard"
+}
 
-# create kidsruby dir
+install_ruby() {
+	echo "Installing Ruby 1.9.2..."
+	tar -xvzf ruby-1.9.2-p290.universal.tar.gz -C "$INSTALLDIR"
+	export PATH="$INSTALLDIR/ruby/bin:$PATH"
+}
 
-# get latest release kidsruby code from repo
+install_kidsruby() {
+	echo "Installing kidsruby editor..."
+	cd "$INSTALLDIR"
+	/usr/local/bin/git clone --branch release git://github.com/hybridgroup/kidsruby.git
+}
 
-# create kidsruby gemset
+install_bundler() {
+	echo "Installing bundler..."
+	gem install bundler
+}
 
-# install bundler
+install_qtbindings() {
+	echo "Installing qtbindings..."
+	gem install qtbindings-4.7.3-universal-darwin-10.gem
+}
 
-# install gems, including the fat binary of qtbindings
+create_install_dir
+install_qt
+install_git
+# install libyaml here?
+install_ruby
+install_bundler
+install_qtbindings
+install_kidsruby
 
-
-
+echo "KidsRuby installation complete. Have fun!"
