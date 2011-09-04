e2b8fa2a8c39c1c36915b2e10a62aafe3f0789e3
deadprogrammer <ron.evans@gmail.com>
2011-09-04 04:23:37 -0700

    Complete remaining install build steps and update README

--- build_installer.sh
+++ build_installer.sh
@@ -21,10 +21,15 @@ build_launcher() {
 	mv build/KidsRuby.app resources
 }
 
+copy_irb() {
+	cp kidsirb.sh resources
+}
+
 build_installer() {
 	/usr/local/bin/platypus -AR  -l -i '/Applications/Platypus.app/Contents/Resources/PlatypusDefault.icns' -a 'KidsRuby Installer for OSX' -o 'Progress Bar' -p '/bin/sh' -u 'The Hybrid Group + Friends'  -V '0.6'  -I 'org.kidsruby.installer' -f '/Users/ron/Developer/kidsrubyinstaller-osx/resources/git-1.7.6-i386-snow-leopard.dmg' -f '/Users/ron/Developer/kidsrubyinstaller-osx/resources/ruby-1.9.2-p290.universal.tar.gz' -f '/Users/ron/Developer/kidsrubyinstaller-osx/resources/yaml-0.1.4.universal.tar.gz' -f '/Users/ron/Developer/kidsrubyinstaller-osx/resources/qt-mac-opensource-4.7.3.dmg' -f '/Users/ron/Developer/kidsrubyinstaller-osx/resources/qtbindings-4.7.3-universal-darwin-10.gem' -f '/Users/ron/Developer/kidsrubyinstaller-osx/resources/kidsruby.tar.gz' -f '/Users/ron/Developer/kidsrubyinstaller-osx/resources/kidsirb.sh' -f '/Users/ron/Developer/kidsrubyinstaller-osx/resources/KidsRuby.app'  -g '#000000'  -b '#ffffff'  -c '/Users/ron/Developer/kidsrubyinstaller-osx/installer.sh' 'build/KidsRubyInstaller.app'
 }
 
 cleanup
 build_launcher
+copy_irb
 build_installer
--- kidsirb.sh
+++ kidsirb.sh
@@ -2,4 +2,6 @@
 export PATH=/Applications/KidsRuby/ruby/bin:$PATH
 export GEM_HOME=/Applications/KidsRuby/ruby/gem:$GEM_HOME
 
-ruby /Applications/KidsRuby/kidsruby/main.rb
+cd kidsruby
+irb
+cd ..
